a06b47b99d891df3148898cbd4f46bf40aa81726
Zev Spitz <shivisi@hotmail.com>
2021-01-10 12:13:03 +0200

    Split documents on heading 1

--- Driver/Program.cs
+++ Driver/Program.cs
@@ -7,34 +7,59 @@ using static System.Console;
 using System.Linq;
 using static ZSpitz.Util.Functions;
 using System.Diagnostics;
+using System.Text.Json;
+using System.Collections.Generic;
+using ZSpitz.Util;
 
-const string pandocPath = @"c:\Program Files\Pandoc\pandoc.exe";
+const string pandocPath = @"""C:\Program Files\Pandoc\pandoc.exe""";
 
 var rootPath = GetFullPath($"{GetParent(GetExecutingAssembly().Location)}");
 var sourcePath = Combine(rootPath, "source");
 var outputPath = Combine(rootPath, "output");
 if (Directory.Exists(outputPath)) {
-    WriteLine("Delete output folder?");
-    if (ReadKey().Key != ConsoleKey.Y) { return; }
+    //WriteLine("Delete output folder?");
+    //if (ReadKey().Key != ConsoleKey.Y) { return; }
     Directory.Delete(outputPath, true);
 }
 CreateDirectory(outputPath);
 
+var filterPath = Combine(rootPath, "Filter.exe");
 foreach (var doc in EnumerateFiles(sourcePath).Where(x => !x.Contains("~$"))) {
-    var name = GetFileNameWithoutExtension(doc);
+    var name = GetFileNameWithoutExtension(doc).ToLower();
     var docRoot = Combine(outputPath, name);
     if (!Directory.Exists(docRoot)) { CreateDirectory(docRoot); }
 
     Process process = new();
     process.StartInfo = new() {
         FileName = "cmd",
-        Arguments = @"/C """"C:\Program Files\Pandoc\pandoc.exe"" -v""",
-        RedirectStandardOutput=true,
-        RedirectStandardError = true
+        Arguments = @$"/C ""{pandocPath} -s {doc} -t json | {filterPath}""",
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        WorkingDirectory = docRoot
     };
     process.EnableRaisingEvents = true;
-    
+
     var result = RunProcess(process);
     WriteLine(result.StdOut);
     WriteLine(result.StdErr);
+
+    var json = File.ReadAllText(Combine(docRoot, "headers.json"));
+    JsonSerializer.Deserialize<Dictionary<int, string>>(json)!.ForEachKVP((pass, id) => {
+        process = new();
+        process.StartInfo = new() {
+            FileName = "cmd",
+            Arguments = @$"/C ""{pandocPath} -s {doc} -t json | {filterPath} --pass={pass} | {pandocPath} -s -f json -t gfm --wrap=preserve --extract-media=. -o {id}.md""",
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            WorkingDirectory = docRoot
+        };
+        process.EnableRaisingEvents = true;
+
+        result = RunProcess(process);
+        WriteLine(result.StdOut);
+        WriteLine(result.StdErr);
+    });
+
+    // while testing, we want to break early
+    break;
 }
--- Filter/Program.cs
+++ Filter/Program.cs
@@ -1,9 +1,79 @@
-﻿using System;
+﻿using System.Collections.Immutable;
+using PandocFilters;
+using PandocFilters.Ast;
+using ZSpitz.Util;
+using System.Linq;
+using System.CommandLine;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.IO;
 
-namespace Filter {
-    class Program {
-        static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+var command = new RootCommand() {
+    new Option<int>("--pass", () => -1)
+};
+var result = command.Parse(args);
+var pass = result.ValueForOption<int>("--pass");
+
+var headerCount = 0;
+
+if (pass == -1) {
+    var headersJsonGenerator = new DelegateVisitor();
+    var headers = new Dictionary<int, string>();
+    headersJsonGenerator.Add((Pandoc pandoc) => {
+        foreach (var block in pandoc.Blocks) {
+            if (block.IsT9) {
+                var header = block.AsT9;
+                if (header.Level==1) {
+                    headerCount += 1;
+                    headers.Add(headerCount, header.Attr.Identifier);
+                }
+            }
+        }
+        return pandoc;
+    });
+    Filter.Run(headersJsonGenerator);
+
+    var jsonString = JsonSerializer.Serialize(headers);
+    File.WriteAllText("headers.json", jsonString);
+    return;
+}
+
+var splitter = new DelegateVisitor();
+splitter.Add((Pandoc pandoc) => {
+    return pandoc with
+    {
+        Blocks = pandoc.Blocks.Select(block => {
+            if (block.IsT9 && block.AsT9.Level == 1) {
+                headerCount += 1;
+            }
+            return (block, headerCount);
+        })
+        .WhereT((_, headerCount) => headerCount==pass)
+        .SelectT((block, _) => block)
+        .ToImmutableList()
+    };
+});
+
+
+var visitor = new DelegateVisitor();
+
+// remove height and width from images, so they'll output as standard markdown images, instead of HTML img elements
+visitor.Add((Image img) => 
+    img with
+    {
+        Attr = img.Attr with
+        {
+            KeyValuePairs = img.Attr.KeyValuePairs.WhereT((key, value) => key.NotIn("height", "width")).ToImmutableList()
         }
     }
-}
+);
+
+// replace underline with emphasis for markdown bold
+visitor.Add((Inline inline) => {
+    if (inline.TryPickT2(out var underline, out var _)) {
+        return new Strong(underline.Inlines);
+    }
+    return inline;
+});
+
+Filter.Run(splitter, visitor);
